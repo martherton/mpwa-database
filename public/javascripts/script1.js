4a25170b246337357409c5065f9b9f48db33affc
Mark Artherton <mark.artherton@gmail.com>
2014-10-22 10:10:34 +0100

    again with js

---
+++ public/javascripts/script1.js
@@ -0,0 +1,41 @@
+window.addEventListener("page:load", initAll, false);
+window.addEventListener("load", initAll, false);
+
+function initAll() {
+	var allLinks = document.getElementById("drophidden");
+
+			allLinks.addEventListener("mouseover",changeText,false);
+			allLinks.addEventListener("mouseout",changeBackText,false);
+	
+	var allLinks2 = document.getElementById("hiddendrop"); 
+
+	
+			allLinks2.addEventListener("mouseover",changeText,false);
+			allLinks2.addEventListener("mouseout",changeBackText,false);
+		
+	var allLinks3 = document.getElementById("suredeletelink");		
+
+			allLinks3.addEventListener("click",showSure,false);
+  
+
+		
+
+}	
+
+function changeText() {
+	
+	
+	document.getElementById("hidcontain").style.display = "block";
+}
+
+function changeBackText() {
+	
+	document.getElementById("hidcontain").style.display = "none";
+}
+
+function showSure() {
+	document.getElementById("suredelete").style.display = "block";
+}
+
+
+
---
+++ public/javascripts/script2.js
@@ -0,0 +1,37 @@
+window.addEventListener("page:load", initAll, false);
+window.addEventListener("load", initAll, false);
+
+function initAll() {
+
+
+var allLinks4 = document.getElementsByTagName("a");		
+			for (var i=0; i<allLinks4.length; i++) {
+		if (allLinks4[i].className.indexOf("dochid") > -1) {
+			allLinks4[i].addEventListener("click",showDel,false);
+		}
+		if (allLinks4[i].className.indexOf("dochid2") > -1) {
+			allLinks4[i].addEventListener("click",hideDel,false);
+		}
+	}
+	
+	var allLinks5 = document.getElementsByClassName("behid");		
+			for (var i=0; i<allLinks5.length; i++) {
+		
+			allLinks5[i].style.display = "none"
+		
+		
+	}		
+
+}	
+
+function showDel() {
+	var clickName = this.id.slice(-1)
+	document.getElementById("downdelhid"+clickName).style.display = "block";
+	event.preventDefault();
+}
+
+function hideDel() {
+	var clickName = this.id.slice(-1)
+	document.getElementById("downdelhid"+clickName).style.display = "none";
+	event.preventDefault();
+}
